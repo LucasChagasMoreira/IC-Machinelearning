e4a0d39d58cd03345210af728f1c08ac67001c82
LucasChagasMoreira <97477970+LucasChagasMoreira@users.noreply.github.com>
2024-02-29 16:59:26 -0300

    torch.optim.adam

--- IMDB/transformer.py
+++ IMDB/transformer.py
@@ -68,7 +68,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
 # Configure o otimizador
-optimizer = AdamW(model.parameters(), lr=1e-5)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
 
 # Treinamento
 epochs = 10
